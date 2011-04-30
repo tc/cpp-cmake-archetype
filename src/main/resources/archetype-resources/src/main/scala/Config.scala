879aaca3a7f8c21b0ff55ef348eff36166b54da2
Tommy Chheng <tommy.chheng@gmail.com>
2011-04-29 18:58:27 -0700

    db source

--- src/main/resources/archetype-resources/src/main/scala/Config.scala
+++ src/main/resources/archetype-resources/src/main/scala/Config.scala
@@ -8,28 +8,10 @@ import java.io.FileInputStream
 import java.io.File
 
 import javax.sql.DataSource
-import com.mysql.jdbc.jdbc2.optional._
 import scala.collection.JavaConversions._
 
 object Config{
   val environment = if(System.getenv("APP_ENV") != null) System.getenv("APP_ENV") else "development"
-
-  def getMySqlDataSource(dbName:String):DataSource = {
-    val dbSettings = getDatabaseSettings(dbName)
-
-    val serverName = dbSettings.getOrElse("host", "localhost").asInstanceOf[String]
-    val port = dbSettings.getOrElse("port", 3306).asInstanceOf[Int]
-    val databaseName = dbSettings.get("database").asInstanceOf[String]
-    val user = dbSettings.getOrElse("username", "root").asInstanceOf[String]
-    val password = dbSettings.get("password").asInstanceOf[String]
-
-    val dataSource = new MysqlConnectionPoolDataSource()
-    dataSource.setUser(user)
-    dataSource.setPassword(password)
-    dataSource.setUrl("jdbc:mysql://%s:%d/%s?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true".format(serverName, port, databaseName))
-    dataSource.setEncoding("UTF-8")
-    dataSource
-  }
 
   def getDatabaseSettings(dbName:String):java.util.HashMap[String,Any] = {
     LoggerFactory.getLogger(getClass.toString).info("Using %s environment for database: %s".format(environment, dbName))
